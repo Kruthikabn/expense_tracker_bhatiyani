293cb099f771fe525a2c31d26c3f57ba01fe3929
Kruthikabn <kruthikabn592@gmail.com>
2025-08-07 18:02:56 +0530

    Fix: added port binding for Render

--- backend/app.py
+++ backend/app.py
@@ -43,6 +43,7 @@ def delete_expense(index):
         return jsonify({'msg': 'Deleted'})
     except IndexError:
         return jsonify({'error': 'Invalid index'}), 404
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
